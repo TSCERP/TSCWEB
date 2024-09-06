dd6544888c06716a030b70a6308dadb18d8c82a4
ntnfit <ntnfit98@gmail.com>
2024-09-06 16:24:15 +0700

    update blog detail

--- resources/views/components/articles/type-badge.blade.php
+++ resources/views/components/articles/type-badge.blade.php
@@ -24,7 +24,7 @@
             'text-violet-500' => $type->color === 'violet',
         ])
     >
-        {!! $type->getIcon() !!}
+        {!! $type->icon !!}
     </div>
 
     {{-- Type Name --}}
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BlogsController;
 use App\Http\Controllers\Articles\ListArticlesController;
+use App\Http\Controllers\Articles\ViewArticleController;
 use Illuminate\Support\Facades\App;
 
 Route::get('/', function () {
@@ -36,8 +37,13 @@ Route::middleware('auth')->group(function () {
 Route::prefix('/blogs')->group(function () {
     Route::get('/', ListArticlesController::class)->name('blogs');
 });
+Route::name('admin.')->prefix('/{blogs:slug}')->group(function () {
+    Route::get('/', ViewArticleController::class)->name('post.show');
+});
 
-//change language
-Route::get('/{post:slug}', [BlogsController::class, 'show'])->name('admin.post.show');
+//Route::name('blogs.')->prefix('/{article:slug}')->group(function () {
+//    Route::get('/', ViewArticleController::class)->name('admin.post.show');
+//});
+
 require __DIR__.'/auth.php';
 
